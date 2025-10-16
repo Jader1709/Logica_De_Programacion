9eccf87f8f8deda7aeb4d5809b9e5744de33c6ea
Jader1709 <jaderbolivar80@gmail.com>
2025-10-16 18:08:31 -0500

    Tarea

---
+++ Ejercicio_52.py
@@ -0,0 +1,24 @@
+VectorA = [] 
+N = int(input("Ingrese el rango del conjunto A: "))
+VectorB = [] 
+M = int(input("Ingrese el rango del conjunto B: "))
+VectorC = []
+x = 0
+
+
+for i in range(N):
+    i += 1
+    NumeroA = float(input(f"Ingrese el numero {i} del conjunto A: "))
+    VectorA.append(NumeroA)
+    VectorC.append(NumeroA)
+for j in range(M):
+    j += 1
+    NumeroB = float(input(f"Ingrese el numero {j} del conjunto B: "))
+    VectorB.append(NumeroB)
+
+
+
+    
+
+
+
